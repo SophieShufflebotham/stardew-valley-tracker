dcfa73ee1e06f7389f26aaa1c0b719fe0ad54777
Tom Cork <tomcork1998@gmail.com>
2020-05-29 11:04:17 +0100

    feat: Added collapsible bar with image.

--- lib/Rooms/RoomsPage.dart
+++ lib/Rooms/RoomsPage.dart
@@ -3,37 +3,49 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/material.dart';
 
 class RoomsPage extends StatelessWidget {
-  final String roomName;
-  RoomsPage(this.roomName);
+  final String _roomName;
+
+  RoomsPage(this._roomName);
 
   @override
   Widget build(BuildContext context) {
-    var titleBar = Stack(
-      children: <Widget>[
-        Container(
-            padding: EdgeInsets.only(left: 10.0),
-            height: MediaQuery.of(context).size.height * 0.5,
-            decoration: new BoxDecoration(
-              image: new DecorationImage(
-                image: new AssetImage("graphics/" + roomName + ".png"), //TODO there's no way this should support spaces - replace them with hyphens?
-                fit: BoxFit.cover,
-              ),
-            )),
-        Container(
-          height: MediaQuery.of(context).size.height * 0.5,
-          padding: EdgeInsets.all(40.0),
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(color: Color.fromRGBO(58, 66, 86, .9)),
-          child: Center(
-            child: Text(roomName),
+    var assetName = _roomName.toLowerCase().replaceAll(' ', '_');
+
+    Widget appBar = SliverAppBar(
+        expandedHeight: 200.0,
+        floating: false,
+        pinned: true,
+        flexibleSpace: FlexibleSpaceBar(
+          centerTitle: true,
+          title: Text(_roomName,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16.0,
+              )),
+          background: Image.asset(
+            "graphics/$assetName.png",
+            fit: BoxFit.cover,
           ),
-        ),
+          collapseMode: CollapseMode.none,
+        ));
+
+    Widget customScrollView = CustomScrollView(
+      slivers: <Widget>[
+        appBar,
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+              (context, index) => Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 75,
+                      color: Colors.black12,
+                    ),
+                  ),
+              childCount: 10),
+        )
       ],
     );
 
-    return Scaffold(
-        body: Column(
-      children: <Widget>[titleBar], //TODO add list-y stuff as big fat widget
-    ));
+    return Scaffold(body: customScrollView);
   }
 }
