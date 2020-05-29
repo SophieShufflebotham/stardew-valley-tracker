48d0299309e1429a4545d3f6e7a4b7c0c2778703
PikaPirate <51209736+PikaPirate@users.noreply.github.com>
2020-05-29 21:25:24 +0100

    Added image to home screen
    Added title image to home screen list

--- lib/src/screens/HomeScreen.dart
+++ lib/src/screens/HomeScreen.dart
@@ -14,15 +14,93 @@ class HomeScreenState extends State<HomeScreen> {
     "Crafts Room",
     "Boiler Room",
   ];
+  static final String roomName = "Community Center";
+  var imageName = roomName.toLowerCase().replaceAll(' ', '_') + "_icon";
+  var sliverTitle = "";
+  var titleBar = "";
+  var controller = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    bool sliverCollapsed = false;
+    bool initialised = false;
+
+    if (!initialised) {
+      sliverTitle = roomName;
+      initialised = true;
+    }
+
+    controller.addListener(() {
+      if (controller.offset > 220 && !controller.position.outOfRange) {
+        if (!sliverCollapsed) {
+          // do what ever you want when silver is collapsing !
+
+          titleBar = roomName;
+          sliverTitle = "";
+          sliverCollapsed = true;
+          setState(() {});
+        }
+      }
+      if (controller.offset <= 220 && !controller.position.outOfRange) {
+        if (sliverCollapsed) {
+          // do what ever you want when silver is expanding !
+
+          sliverTitle = roomName;
+          titleBar = "";
+          sliverCollapsed = false;
+          setState(() {});
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Stardew Valley Checklist"),
-      ),
-      body: _buildList(),
+    var assetName = roomName.toLowerCase().replaceAll(' ', '_');
+
+    Widget appBar = SliverAppBar(
+        expandedHeight: 200.0,
+        floating: false,
+        pinned: true,
+        title: Text(titleBar,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16.0,
+            )),
+        flexibleSpace: Stack(
+          children: <Widget>[
+            Positioned.fill(
+              child: ColorFiltered(
+                  colorFilter: ColorFilter.mode(
+                      Colors.grey.withOpacity(0.7), BlendMode.modulate),
+                  child: Image.asset("graphics/$assetName.png",
+                      fit: BoxFit.cover)),
+            ),
+            FlexibleSpaceBar(
+                title: Text(sliverTitle,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                    ))),
+          ],
+        ));
+
+    Widget customScrollView = CustomScrollView(
+      controller: controller,
+      slivers: <Widget>[
+        appBar,
+        SliverList(
+          delegate: SliverChildBuilderDelegate((context, index) {
+            if (index < _rooms.length) {
+              return _buildListItem(context, index);
+            }
+          }),
+        )
+      ],
     );
+
+    return Scaffold(body: customScrollView);
   }
 
   Widget _buildList() {
@@ -31,16 +109,15 @@ class HomeScreenState extends State<HomeScreen> {
   }
 
   Widget _buildListItem(BuildContext context, int i) {
-    var roomName = _rooms[i];
-    var imageName = roomName.toLowerCase().replaceAll(' ', '_') + "_icon";
-
+    var newRoomName = _rooms[i];
+    var newImageName = newRoomName.toLowerCase().replaceAll(' ', '_') + "_icon";
     return ListItem(
-      name: roomName,
-      iconImage: AssetImage("graphics/$imageName.png"),
+      name: newRoomName,
+      iconImage: AssetImage("graphics/$newImageName.png"),
       onTap: () {
         Navigator.of(context).push(
           MaterialPageRoute<void>(
-            builder: (context) => RoomScreen(roomName: roomName),
+            builder: (context) => RoomScreen(roomName: newRoomName),
           ),
         );
       },
