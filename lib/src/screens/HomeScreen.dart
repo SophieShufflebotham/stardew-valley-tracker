f800db693fda898d5ecfbd061c9e5bfcc6de162b
Tom Cork <tomcork1998@gmail.com>
2020-05-31 15:01:19 +0100

    Refactored home screen

--- lib/src/screens/HomeScreen.dart
+++ lib/src/screens/HomeScreen.dart
@@ -16,109 +16,30 @@ class HomeScreenState extends State<HomeScreen> {
   List<Room> _rooms = new List<Room>();
 
   HomeScreenState() {
-    getInitStatus();
+    initialiseDatabase();
   }
 
   static final String pageTitle = "Community Center";
   var imageName = pageTitle.toLowerCase().replaceAll(' ', '_') + "_icon";
   var sliverTitle = "";
   var titleBar = "";
-  var controller = ScrollController();
-
-  @override
-  void initState() {
-    super.initState();
-    bool sliverCollapsed = false;
-    bool initialised = false;
-
-    if (!initialised) {
-      sliverTitle = pageTitle;
-      initialised = true;
-    }
-
-    controller.addListener(() {
-      if (controller.offset > 220 && !controller.position.outOfRange) {
-        if (!sliverCollapsed) {
-          // do what ever you want when silver is collapsing !
-
-          titleBar = pageTitle;
-          sliverTitle = "";
-          sliverCollapsed = true;
-          setState(() {});
-        }
-      } else if (controller.offset <= 220 && !controller.position.outOfRange) {
-        if (sliverCollapsed) {
-          // do what ever you want when silver is expanding !
-
-          sliverTitle = pageTitle;
-          titleBar = "";
-          sliverCollapsed = false;
-          setState(() {});
-        }
-      }
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    var assetName = pageTitle.toLowerCase().replaceAll(' ', '_');
-
-    Widget appBar = SliverAppBar(
-        expandedHeight: 200.0,
-        floating: false,
-        pinned: true,
-        title: Text(titleBar,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 16.0,
-            )),
-        flexibleSpace: Stack(
-          children: <Widget>[
-            Positioned.fill(
-              child: ColorFiltered(
-                  colorFilter: ColorFilter.mode(
-                      Colors.grey.withOpacity(0.7), BlendMode.modulate),
-                  child: Image.asset("graphics/$assetName.png",
-                      fit: BoxFit.cover)),
-            ),
-            FlexibleSpaceBar(
-                title: Text(sliverTitle,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16.0,
-                    ))),
-          ],
-        ));
-
-    Widget customScrollView = CustomScrollView(
-      controller: controller,
-      slivers: <Widget>[
-        appBar,
-        SliverList(
-          delegate: SliverChildBuilderDelegate((context, index) {
-            if (index < _rooms.length) {
-              return _buildListItem(context, index);
-            }
-          }),
-        )
-      ],
+    return Scaffold(
+      appBar: AppBar(title: Text('Community Center')),
+      body: ListView.builder(
+        itemCount: _rooms.length,
+        itemBuilder: _buildListItem,
+      ),
     );
-
-    return Scaffold(body: customScrollView);
-  }
-
-  Widget _buildList() {
-    Widget retVal =
-        ListView.builder(itemCount: _rooms.length, itemBuilder: _buildListItem);
-
-    return retVal;
   }
 
   Widget _buildListItem(BuildContext context, int i) {
-    var roomName = _rooms[i].name;
+    var room = _rooms[i];
     return ListItem(
-      name: roomName,
-      iconImage: AssetImage("graphics/placeholder.png"),
+      name: room.name,
+      iconImage: AssetImage(room.iconPath),
       onTap: () {
         Navigator.of(context).push(
           MaterialPageRoute<void>(
@@ -137,21 +58,15 @@ class HomeScreenState extends State<HomeScreen> {
     });
   }
 
-  void getInitStatus() async {
+  void initialiseDatabase() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    bool isInitialised = false;
-    isInitialised =
-        (prefs.getBool('dbReady') == null) ? false : prefs.getBool('dbReady');
 
-    print("getBool: " + prefs.getBool('dbReady').toString());
-
-    if (!isInitialised) {
+    // Check if this is the first run of the application.
+    if (!(prefs.getBool('dbReady') == true)) {
       bool success = await PopulateDb().initialiseDatabase();
-
-      if (success) {
-        prefs.setBool('dbReady', true);
-      }
+      prefs.setBool('dbReady', success);
     }
+
     getDatabaseContent();
   }
 }
